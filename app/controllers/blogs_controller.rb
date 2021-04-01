f516a044c58c4b757ee80a42b71a5fdcd3591fd7
nishinotakaya <takaya314boxing@gmail.com>
2021-04-01 19:42:36 +0900

    fix/ちがう月も反映

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,11 @@
 class BlogsController < ApplicationController
   def index
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @blogs = Blog.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week) 
+  end
+
+  def carendar_top
+    @blog = Blog.where(id: 1)
     @first_day = params[:date].nil? ? 
     Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
@@ -40,37 +46,56 @@ class BlogsController < ApplicationController
 
   def all_blogs_edit
     @first_day = params[:date].nil? ? 
-    Date.current.beginning_of_month : params[:date].to_date
+    Date.current.next_month.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
+    now = Time.current 
     one_month = [*@first_day..@last_day]
-     @blogs = Blog.where(start_time: @first_day..@last_day).group(:start_time)
+     @blogs = Blog.where(start_time: @first_day..@last_day).group(:start_time).order(:start_time)
      unless one_month.count == @blogs.count
        ActiveRecord::Base.transaction do 
          one_month.each { |day| @blogs.create!(start_time: day) }
        end
-       @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time).group(:start_time)
+       @blogs = Blog.where(start_time: @first_day..@last_day).group(:start_time)
      end
    end  
  
    def all_blogs_update
      ActiveRecord::Base.transaction do # トランザクションを開始します。
+        @blog = Blog.where(id: 1)
+        @blog.update(not_content_birthday_people)
        all_blogs_parameter.each do |id, item|
          blog = Blog.find(id)
-         blog.update_attributes!(item)  
+         blog.update_attributes!(item)
        end
      end
      flash[:success] = "レクカレンダーを更新しました"
      redirect_to blogs_url      
    end  
+
+   def blogs_birthday
+   end
+
  
    private
  
    def blog_parameter
-     params.require(:blog).permit(:title, :content, :start_time)
+     params.require(:blog).permit(:title, :content, :start_time, :not_content_birthday_people)
    end
+
+  def not_content_birthday_people
+    params.require(:blog).permit(:not_content_birthday_people)
+  end
  
    def all_blogs_parameter
-     params.require(:blog).permit(blogs: [:title, :content, :start_time])[:blogs]
+     params.require(:blog).permit(blogs: [:title, :content, :start_time, :not_content_birthday_people])[:blogs]
    end
+
+   def current_blog
+    @current_blog ||= Blog.find_by(id: session[:id])
+  end
+
+  def start_time
+    self.my_related_model.start ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  end
  
 end
