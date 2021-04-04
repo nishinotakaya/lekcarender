9d1fd64d8ae815eb29f6b2507fa95949c1b21173
nishinotakaya <takaya314boxing@gmail.com>
2021-04-04 18:10:40 +0900

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root to: 'blogs#index'
   resources :blogs do
-    member do 
-      get 'all_blogs_edit'
-      patch 'all_blogs_update'
-    end  
+    get 'all_blogs_edit', on: :member
+    patch 'all_blogs_update', on: :member
   end  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
