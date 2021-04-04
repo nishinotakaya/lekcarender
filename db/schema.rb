705d71ac1ab31d15c2759811a7463a7f07be0a88
nishinotakaya <takaya314boxing@gmail.com>
2021-04-04 18:45:30 +0900

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,9 @@ ActiveRecord::Schema.define(version: 2021_03_06_090316) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.text "content_1"
+    t.text "content_2"
+    t.text "content_3"
     t.datetime "start_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
