d465566fc8dde74c63f21cada2c87bd2b24cdb07
nishinotakaya <takaya314boxing@gmail.com>
2021-04-06 22:41:26 +0900

    group化

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -54,13 +54,13 @@ class BlogsController < ApplicationController
 		Date.current.next_month.beginning_of_month : params[:date].to_date
 		@last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day]
-        @blogs = current_user.blogs.where(start_time: @first_day..@last_day).group(:start_time)
+        @blogs = current_user.blogs.where(start_time: @first_day..@last_day).group(:start_time).group("blogs.id")
         unless one_month.count == @blogs.count
           ActiveRecord::Base.transaction do 
             one_month.each { |day| @blogs.create!(start_time: day) }
           end
         end
-    @blogs = current_user.blogs.where(start_time: @first_day..@last_day).group(:start_time)
+    @blogs = current_user.blogs.where(start_time: @first_day..@last_day).group(:start_time).group("blogs.id")
    end  
  
    def all_blogs_update
