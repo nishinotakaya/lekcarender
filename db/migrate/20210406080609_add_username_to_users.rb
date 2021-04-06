2cbb4a8f9bc53c470ef512f7e69bf72809ea6f66
nishinotakaya <takaya314boxing@gmail.com>
2021-04-06 19:45:36 +0900

    fix/clum追加

--- db/migrate/20210406080609_add_username_to_users.rb
+++ db/migrate/20210406080609_add_username_to_users.rb
@@ -1,6 +1,5 @@
 class AddUsernameToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :username, :string
-    t.references :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_095807) do
+ActiveRecord::Schema.define(version: 2021_04_06_080609) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_04_06_095807) do
     t.date "start_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
