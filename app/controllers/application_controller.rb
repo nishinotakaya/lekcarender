14a03cb4cc73b34450e9223f968bad21ece416a1
nishinotakaya <takaya314boxing@gmail.com>
2021-04-06 21:08:17 +0900

    ログイン後変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    blogs_path(resource)
+    all_blogs_edit_blog_path(resource)
   end
 
   private
