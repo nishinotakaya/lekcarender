8590a0f94b91c35db74d3a4890a82f8ef2390d53
nishinotakaya <takaya314boxing@gmail.com>
2021-04-07 13:00:01 +0900

    UPdate gem for heroku

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -54,13 +54,13 @@ class BlogsController < ApplicationController
 		Date.current.beginning_of_month : params[:date].to_date
 		@last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day]
-      @blogs = current_user.blogs.where(start_time: @first_day..@last_day)
-        if one_month.count == @blogs.count
+      @blogs = current_user.blogs.where(start_time: @first_day..@last_day).order(:start_time)
+      unless one_month.count == @blogs.count
           ActiveRecord::Base.transaction do 
             one_month.each { |day| @blogs.create!(start_time: day) }
           end
         end
-      @blogs = current_user.blogs.where(start_time: @first_day..@last_day).select("blogs.id", "start_time", "title", "content").group("blogs.id", :start_time)
+      @blogs = current_user.blogs.where(start_time: @first_day..@last_day).select("blogs.id", "start_time", "title", "content").group(:start_time)
    end  
  
    def all_blogs_update
