de421ff9986f0a6adac99171caa7b4acab7f585c
nishinotakaya <takaya314boxing@gmail.com>
2021-04-15 23:02:28 +0900

    fix/一通りレイアウト整える

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -51,14 +51,14 @@ class BlogsController < ApplicationController
     Date.current.next_month.beginning_of_month : params[:date].to_date
 		@last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day]
-      @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time)
-      unless one_month.count == @blogs.count || @blogs.present?  
-          ActiveRecord::Base.transaction do 
-            one_month.each { |day| @blogs.create!(start_time: day) }
-          end
-        end
-      @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time)
-   end  
+    @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time)
+    unless one_month.count == @blogs.count || @blogs.present?  
+      ActiveRecord::Base.transaction do 
+        one_month.each { |day| @blogs.create!(start_time: day) }
+      end
+    end
+    @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time)
+  end  
  
    def all_blogs_update
      ActiveRecord::Base.transaction do # トランザクションを開始します。
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,8 @@
 class Client < ApplicationRecord
 
+
+  validates :name, presence: true
+
   before_save do
     self.use_day.gsub!(/[\[\]\"]/, "") if attribute_present?("use_day")
   end
