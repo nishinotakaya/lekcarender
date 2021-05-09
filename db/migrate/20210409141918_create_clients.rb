791d1494caa8c18e53525c121f2d2b61eb7c9307
nishinotakaya <takaya314boxing@gmail.com>
2021-05-10 01:18:29 +0900

    レイアウト更新

--- db/migrate/20210409141918_create_clients.rb
+++ db/migrate/20210409141918_create_clients.rb
@@ -2,6 +2,8 @@ class CreateClients < ActiveRecord::Migration[6.0]
   def change
     create_table :clients do |t|
       t.string :name
+      t.string :name_h
+      t.string :sex
       t.date :birthday
       t.string :use_day
       t.string :string
