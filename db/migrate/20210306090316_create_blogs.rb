f77f1aef281d567a4318eb78019a5eb13963196f
takayanishino <takaya314boxing@gmail.com>
2021-05-28 20:22:15 +0900

    fix/belong_to

--- db/migrate/20210306090316_create_blogs.rb
+++ db/migrate/20210306090316_create_blogs.rb
@@ -1,12 +1,6 @@
 class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
-      t.string :title
-      t.text :content
-      t.text :content_1
-      t.text :content_2
-      t.text :content_3
-      t.date :start_time
     end
   end
 end
--- db/migrate/20210528111347_blogs.rb
+++ db/migrate/20210528111347_blogs.rb
@@ -1,6 +1,12 @@
-class Blogs < ActiveRecord::Migration[6.0]
+class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
+      t.string :title
+      t.text :content
+      t.text :content_1
+      t.text :content_2
+      t.text :content_3
+      t.date :start_time
       t.timestamps
       t.references :user, foreign_key: true
     end  
