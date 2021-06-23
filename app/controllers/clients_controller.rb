976fa231d216655b8a96c20756462ee971587643
nishinotakaya <takaya314boxing@gmail.com>
2021-06-23 19:50:31 +0900

    fix/編集完了

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,7 +4,7 @@ class ClientsController < ApplicationController
   # GET /clients or /clients.json
   def index
     @search_params = client_search_params
-    @clients = Client.search(@search_params).order(:name_h)
+    @clients = current_user.clients.where(user_id: current_user.id).search(@search_params).order(:name_h)
   end
 
   # def text_index
@@ -17,7 +17,7 @@ class ClientsController < ApplicationController
 
   # GET /clients/new
   def new
-    @client = Client.new
+    @client = current_user.clients.new
   end
 
   # GET /clients/1/edit
@@ -26,8 +26,8 @@ class ClientsController < ApplicationController
 
   # POST /clients or /clients.json
   def create
-    @client = Client.new(client_params)
-
+    @client = current_user.clients.new(client_params)
+    debugger
     respond_to do |format|
       if @client.save
         format.html { redirect_to @client, notice: "Client was successfully created." }
@@ -61,7 +61,7 @@ class ClientsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_client
-      @client = Clients.find(params[:id])
+      @client = current_user.clients.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
@@ -70,6 +70,6 @@ class ClientsController < ApplicationController
     end
 
     def client_search_params
-      params.fetch(:search, {}).permit(:name, :birthday, :use_day)
+      params.fetch(:search, {}).permit(:name, :birthday, :use_day).merge(user_id: params[:user_id])
     end
 end
