01325c3c210a8748741f0caf4d6813661b7e6ec9
nishinotakaya <takaya314boxing@gmail.com>
2021-06-23 20:51:36 +0900

    fix/heroku 更新

--- db/migrate/20210528111347_create_blogs.rb
+++ db/migrate/20210528111347_create_blogs.rb
@@ -2,7 +2,7 @@ class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
       t.string :title
-      t.text :content
+      t.text :contentheroku
       t.text :content_1
       t.text :content_2
       t.text :content_3
