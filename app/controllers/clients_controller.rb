c55805a760640e8f01a0df477e9bbbb80b49f0c0
nishinotakaya <takaya314boxing@gmail.com>
2021-06-29 19:30:46 +0900

    fix/曜日修正

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -60,7 +60,7 @@ class ClientsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_client
-      @client = Clients.find(params[:id])
+      @client = Client.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
