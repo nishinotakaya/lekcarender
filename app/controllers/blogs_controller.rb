f106ab2bd459bbd16a67fd09612641a696a30d6b
nishinotakaya <takaya314boxing@gmail.com>
2021-07-20 06:00:31 +0900

    fix/aaa

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -36,7 +36,7 @@ class BlogsController < ApplicationController
 
   def edit
     @start_date = params.fetch(:start_date, Date.today).to_date
-    @blog = current_user.blogs.find(params[:id])
+    @blog = Blog.find(params[:id])
   end
 
   def update
@@ -53,13 +53,13 @@ class BlogsController < ApplicationController
     Date.current.next_month.beginning_of_month : params[:date].to_date
 		@last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day]
-    @blogs = current_user.blogs.where(start_time: @first_day..@last_day).order(:start_time)
+    @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time)
     unless one_month.count == @blogs.count || @blogs.present?  
       ActiveRecord::Base.transaction do 
         one_month.each { |day| @blogs.create!(start_time: day) }
       end
     end
-    @blogs = current_user.blogs.where(start_time: @first_day..@last_day).order(:start_time)
+    @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time)
   end  
  
   def all_blogs_update
