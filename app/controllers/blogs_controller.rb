c5c1e1e465f1b6d1c4859e53895ff3cb95013068
nishinotakaya <takaya314boxing@gmail.com>
2021-07-20 06:03:32 +0900

    fix/aaa

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@ class BlogsController < ApplicationController
   
   def index
     @start_date = params.fetch(:start_date, Date.today).to_date
-    @blogs =  Blog.where(start_time: @start_date.beginning_of_month..@start_date.end_of_month)
+    @blogs =  current_user.blogs.where(start_time: @start_date.beginning_of_month..@start_date.end_of_month)
     @clients = Client.all
   end
 
@@ -36,7 +36,7 @@ class BlogsController < ApplicationController
 
   def edit
     @start_date = params.fetch(:start_date, Date.today).to_date
-    @blog = Blog.find(params[:id])
+    @blog = current_user.blogs.find(params[:id])
   end
 
   def update
@@ -53,13 +53,13 @@ class BlogsController < ApplicationController
     Date.current.next_month.beginning_of_month : params[:date].to_date
 		@last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day]
-    @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time)
+    @blogs = current_user.blogs.where(start_time: @first_day..@last_day).order(:start_time)
     unless one_month.count == @blogs.count || @blogs.present?  
       ActiveRecord::Base.transaction do 
         one_month.each { |day| @blogs.create!(start_time: day) }
       end
     end
-    @blogs = Blog.where(start_time: @first_day..@last_day).order(:start_time)
+    @blogs = current_user.blogs.where(start_time: @first_day..@last_day).order(:start_time)
   end  
  
   def all_blogs_update
