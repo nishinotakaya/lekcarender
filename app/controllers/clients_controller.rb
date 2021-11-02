03047885f7eb53f34f02a2fe6c2883164024337b
nishino <takaya314boxing@gmail.com>
2021-11-03 01:56:45 +0900

    fix/master

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,10 +1,17 @@
 class ClientsController < ApplicationController
   before_action :set_client, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
+  require 'csv'
   # GET /clients or /clients.json
   def index
     @search_params = client_search_params
-    @clients = Client.search(@search_params).order(:name_h)
+    @clients = Client.where(user_id: current_user.id).search(@search_params).order(:name_h)
+    respond_to do |format|
+      format.html
+      format.csv do |csv|
+        send_clients_csv(@clients)
+      end
+    end  
   end
 
   # def text_index
@@ -26,7 +33,7 @@ class ClientsController < ApplicationController
 
   # POST /clients or /clients.json
   def create
-    @client = current_user.clients.new(client_params)
+    @client = Client.new(client_params)
     respond_to do |format|
       if @client.save
         format.html { redirect_to @client, notice: "Client was successfully created." }
@@ -58,9 +65,31 @@ class ClientsController < ApplicationController
   end
 
   private
+  
+    def send_clients_csv(clients)
+      csv_data = CSV.generate(row_sep: "\r\n", encoding:Encoding::CP932)  do |csv|
+        header = %w(名前 性別 誕生日 年齢 利用日)
+        csv << header
+        clients.each do |client|
+          values = [
+            client.name,
+            client.sex,
+            client.birthday.strftime("%Y年 %m月 %d日"),
+            age(client.birthday),
+            client.use_day
+          ]
+          csv << values
+        end
+      end
+      send_data(csv_data, filename: "clients.csv")
+    end
+
+    def age(birthday)
+      (Date.today.strftime('%Y%m%d').to_i - birthday.strftime('%Y%m%d').to_i) / 10000
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_client
-      @client = Client.find(params[:id])
+      @client = current_user.clients.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
