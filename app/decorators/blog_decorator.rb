ad395716f0666aa51c2de3ed4ef151347a2016e1
nishinotakaya <takaya314boxing@gmail.com>
2021-12-13 22:57:14 +0900

    fix/デコレーター追加

--- app/decorators/blog_decorator.rb
+++ app/decorators/blog_decorator.rb
@@ -9,7 +9,28 @@ module BlogDecorator
     link_to blog_title, edit_blog_path(self), class: "carender_lek_class"
   end
 
-  def blog_content_all
+  def title_a
+    if self.title.present?
+      self.title
+    end   
+  end
+
+  def content
+    if content_1.present? || self.title.present?
+      content_1
+    end   
+  end
+
+  def content_a
+    if self.content_2.present? || self.title.present?
+      content_2
+    end   
+  end
+
+  def content_b
+    if self.content_3? || self.title.present?
+      content_3
+    end   
   end
   
 end
